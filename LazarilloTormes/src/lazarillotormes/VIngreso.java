6a04a8de4e8826279a3c2182025ae5e4fb640bda
jesusroncerodam <44838341+jesusroncerodam@users.noreply.github.com>
2018-11-12 16:34:50 +0100

    cccc

---
+++ LazarilloTormes/src/lazarillotormes/VIngreso.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vistas;
+
+import controladores.ContrIngreso;
+import trabajodi.Logica;
+
+/**
+ *
+ * @author Guille
+ */
+public class VIngreso {
+    private ContrIngreso controlador;
+
+    public VIngreso(Logica logica) {
+        controlador=new ContrIngreso(this, logica);
+    }
+    
+}
